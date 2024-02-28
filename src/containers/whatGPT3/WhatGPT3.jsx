d4f3917e86fa168ad7d47815e0fdfd5c2b81aa64
MK3000 <kristofferssonmilton@gmail.com>
2024-02-28 20:44:16 +0100

    Images, Navbar, Heading, Brand

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './whatGPT3.css';
+import { Feature } from '../../components';
 
 const WhatGPT3 = () => {
   return (
-    <div>
-        WhatGPT3
+    <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p> Explore The Library </p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   )
 }
