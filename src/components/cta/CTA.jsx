735b466ce8ac9c7d438339eafe4ebd2e6fad773d
MK3000 <kristofferssonmilton@gmail.com>
2024-03-05 11:05:00 +0100

    Article, CTA, Blog & Footer

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,10 +3,21 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>
-        CTA
+    <div className='gpt3__cta'>
+        <div className="gpt3__cta-content">
+          <p> 
+            Request Early Access To Get Started
+          </p>
+          <h3> Register Today & Start Exploring</h3>
+        </div>
+        <div className="gpt3__cta-btn">
+          <button type='button'> Get Started </button>
+        </div>
     </div>
   )
 }
 
 export default CTA
+
+
+// Click To Action
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg';
 
 const Footer = () => {
   return (
-    <div>
-        Footer
+    <div className='gpt3__footer section__padding'>
+        <div className="gpt3__footer-heading">
+          <h1 className='gradient__text'> Do you want to step into the future before others </h1>
+        </div>
+        <div className="gpt3__footer-btn">
+          <p> Request Early Access </p>
+        </div>
+
+        <div className="gpt3__footer-links">
+          <div className="gpt3__footer-links_logo">
+            <img src={gpt3Logo} alt='logo'/>
+            <p> Pilgatan 21, 112 23 SWE Stockholm, All Rights Reserved </p>
+          </div>
+          <div className="gpt3__footer-links_div">
+            <h4> Links </h4>
+            <p>GitHub</p>
+            <p>Social Media</p>
+            <p>Counters</p>
+            <p>Contact</p>
+          </div>
+          <div className="gpt3__footer-links_div">
+            <h4> Company </h4>
+            <p>Terms & Conditions</p>
+            <p>Privacy Policy</p>
+            <p>Contact</p>
+          </div>
+          <div className="gpt3__footer-links_div">
+            <h4> Get in touch </h4>
+            <p>Pilgatan 21 112 23 SWE Stockholm</p>
+            <p>+46723423550</p>
+            <p>info@gpt3.com</p>
+          </div>
+        </div>
+
+        <div className="gpt3__footer-copyright">
+          <p>© 2024 GPT-3. All rights reserved. </p>
+        </div>
     </div>
   )
 }
